97846061a38f8345b248b3f7ee6ec698028de562
afeefakvt <afeefakvt6@gmail.com>
2025-08-23 13:00:36 +0530

    chore: setup backend (Express+TS) and frontend (Next.js+TS)

---
+++ backend/src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import connectDB from './config/db';
+
+const app = express();
+
+connectDB();
+
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true
+}));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+
+const port = process.env.PORT || 5000;
+
+app.get('/',(req,res)=>{
+    res.send("Hello")
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+    
+})
