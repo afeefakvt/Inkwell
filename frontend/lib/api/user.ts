c9cfcf5dc68c8f2298c426cd7f283b5411acf10d
afeefakvt <afeefakvt6@gmail.com>
2025-08-24 21:20:29 +0530

    feat: user profile page with user details and blog management

---
+++ frontend/lib/api/user.ts
@@ -0,0 +1,32 @@
+import { axiosInstance } from "./axiosInstance";
+
+export const updateProfile = async ( data: { name: string; email: string }) => {
+    try {
+        const res = await axiosInstance.put('/profile', data)
+        return res.data;
+    } catch (error:any) {
+        throw error.response?.data || new Error("Something went wrong");        
+    }
+ 
+};
+
+export const changePassword = async(data: { currentPassword: string; newPassword: string; confirmNewPassword: string })=>{
+    try {
+        const res = await axiosInstance.put('/changePassword',data)
+        return res.data
+    } catch (error:any) {
+        throw error.response?.data || new Error("Something went wrong");        
+
+    }
+}
+
+
+export const getMyBlogs = async()=>{
+    try {
+        const response = await axiosInstance.get('/myBlogs')
+        return response.data
+    } catch (error:any) {
+        throw error.response?.data || new Error("Something went wrong");        
+
+    }
+}
